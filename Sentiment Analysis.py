8ddbba5857f3f09236cb4d9ab00c13b6c1d52de3
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2024-04-24 11:17:36 -0700

    Speech TS sentiment testing

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -3,6 +3,8 @@ import sys
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
 
 cwd = os.getcwd()
 # Find and import config file
@@ -33,7 +35,7 @@ sp500["date"] = pd.to_datetime(sp500["date"])
 minutes = minutes[["date", "sentiment"]]
 minutes = minutes.rename(columns={"sentiment": "minute_sentiment"})
 minutes["date"] = pd.to_datetime(minutes["date"])
-
+minutes2 = minutes.copy(deep=True)
 minutes = minutes.groupby("date").mean().reset_index()
 
 filter_df = minutes.copy(deep=True)
@@ -65,4 +67,50 @@ minutes = minutes.groupby(minutes["event"]).mean().reset_index()
 
 minutes = pd.merge(minutes, temp, how="left", left_on="event", right_on="event")
 
-print(minutes.head())
+print(minutes2.head(5))
+
+minutes2["dup_number"] = minutes2.groupby(["date"]).cumcount() + 1
+pivot = pd.pivot_table(
+    minutes2,
+    index="dup_number",
+    columns="date",
+    values="minute_sentiment",
+    fill_value=None,
+)
+print(pivot)
+
+window = 10
+pivot = pivot.iloc[:100]
+
+
+pivot["mean"] = pivot.mean(axis=1)
+pivot["var"] = pivot.var(axis=1)
+pivot["sd"] = pivot["var"] ** 0.5
+pivot["ci"] = 1.96 * np.sqrt(pivot["sd"] / len(pivot["sd"]))
+
+pivot["rmean"] = pivot["mean"].rolling(window).mean()
+pivot["rvar"] = pivot["mean"].rolling(window).var()
+pivot["rsd"] = pivot["rvar"] ** 0.5
+# pivot["ci"] = 1.96 * np.sqrt(pivot["rsd"] / len(pivot["rsd"]))
+
+
+pivot["count"] = pivot.count(axis=1, numeric_only=True)
+
+fig, axs = plt.subplots(2, 1, figsize=(20, 10))
+axs[0].plot(pivot["mean"], color="blue", label="mean")
+axs[0].fill_between(
+    pivot.index,
+    pivot["mean"] - pivot["ci"],
+    pivot["mean"] + pivot["ci"],
+    color="b",
+    alpha=0.1,
+)
+axs[1].plot(pivot["count"], color="red", label="standard deviation")
+
+axs[0].set_title(f"Mean Tone By Line Number: {window} line window", fontsize=40)
+axs[1].set_title(
+    f"Standard Deviation of Tone By Line Number: {window} line window", fontsize=40
+)
+plt.show()
+
+pivot.to_csv("/Users/kylenabors/Downloads/pivot.csv")
